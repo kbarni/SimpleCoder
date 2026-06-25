8f70bb173f4bf374add780309cc02a91b87676a1
Barna Keresztes <barna.keresztes@ims-bordeaux.fr>
2026-06-25 20:14:03 +0200

    Windows version fix

--- src/util/Log.cpp
+++ src/util/Log.cpp
@@ -26,7 +26,11 @@ std::string timestamp() {
   const auto now = std::chrono::system_clock::now();
   const std::time_t t = std::chrono::system_clock::to_time_t(now);
   std::tm tm{};
+#ifdef _WIN32
+  localtime_s(&tm, &t);
+#else
   localtime_r(&t, &tm);
+#endif
   char buf[32];
   std::strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", &tm);
   return buf;
